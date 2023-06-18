1908cb071702a8d54e09ee4c38929a829cfd0286
Rômulo Henri <123867521+romhenri@users.noreply.github.com>
2023-06-18 16:17:52 -0300

    fix: Slider (NotWorking)

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import styles from './css/style.css'
 // Sections
 import SecStart from './layout/SecStart'
 import SecForm from './layout/SecForm'
@@ -7,6 +6,7 @@ import SecImg from './layout/SecImg'
 import SecData from './layout/SecData'
 // Components
 import Footer from './components/Footer'
+import styles from './css/style.css?inline'
 
 // Componentes Snippet = "rafce"
 
--- src/layout/SecImg.jsx
+++ src/layout/SecImg.jsx
@@ -1,37 +1,64 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Title from '../components/Title'
 import Img from '../components/Img'
+import styles from '../css/style.css'
 
 import pathImg from '../assets/img/292030_20220314153108_1.png'
-import pathImg1 from '../assets/img/292030_20220305083107_1.png'
+// import pathImg1 from '../assets/img/292030_20220305083107_1.png'
 import pathArrowLeft from '../assets/svg/arrow-left-solid.svg'
 
-var indexImg = 0
-
-if (indexImg == 0) {
-	var img = pathImg
-} else {
-	var img = pathImg1
-}
-
 const SecImg = () => {
-	function nextImage() {
-		++indexImg
+	const [indexImg, setIndexImg] = useState(0)
+	const [img, setImg] = useState(pathImg)
+
+	// var img = indexImg
+
+	const reloadImage = () => {
+		if (indexImg == 0) {
+			setImg(pathImg)
+		} else if (indexImg == 1) {
+			setImg(pathImg1)
+		}
 	}
 
-	function prevImage() {
-		--indexImg
+	const nextImage = () => {
+		if (indexImg == 1) {
+			setIndexImg(0)
+		} else {
+			setIndexImg(indexImg + 1)
+		}
+
+		setImg(pathImg1)
+		reloadImage()
 	}
+
+	const prevImage = () => {
+		if (indexImg == 0) {
+			setIndexImg(1)
+		} else {
+			setIndexImg(indexImg - 1)
+		}
+		reloadImage()
+	}
+
+	// function prevImage() {
+	// 	--indexImg
+	// }
 
 	return (
 		<section>
 			<Title>Imagens</Title>
 			<div className="imgBox">
-				<aside onClick={prevImage()}>
+				<aside onClick={prevImage} alt="Mudar">
 					<img src={pathArrowLeft} alt="Mudar" />
+					{indexImg + 1}
 				</aside>
-				<Img src={img} alt={'Geralt de Rívia em regiões gélidas.'} />
-				<aside className="right" onClick={nextImage()}>
+				<Img
+					src={img}
+					alt={'Geralt de Rívia em regiões gélidas.' + indexImg}
+				/>
+				{img}
+				<aside className="right" onClick={nextImage}>
 					<img src={pathArrowLeft} alt="Mudar" />
 				</aside>
 			</div>
