a86b6bf8387ceea5147c43b2071ee62f9c2825fa
Rômulo Henri <123867521+romhenri@users.noreply.github.com>
2023-06-18 15:43:51 -0300

    feat: Slider

--- src/layout/SecImg.jsx
+++ src/layout/SecImg.jsx
@@ -3,12 +3,38 @@ import Title from '../components/Title'
 import Img from '../components/Img'
 
 import pathImg from '../assets/img/292030_20220314153108_1.png'
+import pathImg1 from '../assets/img/292030_20220305083107_1.png'
+import pathArrowLeft from '../assets/svg/arrow-left-solid.svg'
+
+var indexImg = 0
+
+if (indexImg == 0) {
+	var img = pathImg
+} else {
+	var img = pathImg1
+}
 
 const SecImg = () => {
+	function nextImage() {
+		++indexImg
+	}
+
+	function prevImage() {
+		--indexImg
+	}
+
 	return (
 		<section>
 			<Title>Imagens</Title>
-			<Img src={pathImg} alt={'Geralt de Rívia em regiões gélidas.'} />
+			<div className="imgBox">
+				<aside onClick={prevImage()}>
+					<img src={pathArrowLeft} alt="Mudar" />
+				</aside>
+				<Img src={img} alt={'Geralt de Rívia em regiões gélidas.'} />
+				<aside className="right" onClick={nextImage()}>
+					<img src={pathArrowLeft} alt="Mudar" />
+				</aside>
+			</div>
 		</section>
 	)
 }
