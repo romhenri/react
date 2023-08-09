4dbc9634df892c5906605ec8857c89a9ac21e90a
Rômulo Henri <romulohenri000@gmail.com>
2023-08-09 09:31:39 -0300

    improve: Counter section

--- src/layout/SecCounter.jsx
+++ src/layout/SecCounter.jsx
@@ -13,24 +13,36 @@ const SecCounter = () => {
 			<Title>Contadores</Title>
 			<div className='container'>
 				<div className='box'>
+				<Button text={'-'} bg="transparent"
+					className="btn-b" 
+					onClick={() => setCount1((count1 => count1 - 1))}/>
 					<div>{count1}</div> 
 					<Button text={'+'} bg="transparent"
 					className="btn-b" 
 					onClick={() => setCount1((count1 => count1 + 1))}/>
 				</div>
 				<div className='box'>
+				<Button text={'-'} bg="transparent"
+					className="btn-b" 
+					onClick={() => setCount2((count2 => count2 - 1))}/>
 					<div>{count2}</div> 
 					<Button text={'+'} bg="transparent"
 					className="btn-b" 
 					onClick={() => setCount2((count2 => count2 + 1))}/>
 				</div>
 				<div className='box'>
+				<Button text={'-'} bg="transparent"
+					className="btn-b" 
+					onClick={() => setCount3((count3 => count3 - 1))}/>
 					<div>{count3}</div> 
 					<Button text={'+'} bg="transparent"
 					className="btn-b" 
 					onClick={() => setCount3((count3 => count3 + 1))}/>
 				</div>
 				<div className='box'>
+				<Button text={'-'} bg="transparent"
+					className="btn-b" 
+					onClick={() => setCount4((count4 => count4 - 1))}/>
 					<div>{count4}</div> 
 					<Button text={'+'} bg="transparent"
 					className="btn-b" 
