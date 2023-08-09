8d6c491849739fde8b3fb91e1e39659ec0590966
Rômulo Henri <romulohenri000@gmail.com>
2023-08-09 07:48:21 -0300

    add: Info section

---
+++ src/layout/SecInfo.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Title from '../components/Title'
+import styles from '../css/SecInfo.module.css'
+
+const SecStart = () => {
+	return (
+		<section className={styles.fadeDown}>
+			<Title>Sobre esse App</Title>
+			<p>Testando...</p>
+		</section>
+	)
+}
+
+export default SecStart
--- src/layout/SecStart.jsx
+++ src/layout/SecStart.jsx
@@ -2,13 +2,21 @@ import React from 'react'
 import Title from '../components/Title'
 import Button from '../components/Button'
 import ButtonsLine from '../components/ButtonsLine'
+import SecInfo from '../layout/SecInfo'
 
 const SecStart = () => {
+	const [active, setActive] = React.useState(false)
+
 	function setDarkMode () {
 		document.body.classList.toggle('dark')
 	}
 
+	function activeSection () {
+		document.body.classList.toggle('dark')
+	}
+
 	return (
+		<>
 		<section>
 			<Title>App React</Title>
 			<p>Esta página tem como intuito testar conhecimentos em React.</p>
@@ -19,9 +27,12 @@ const SecStart = () => {
 					className="btn-b"
 					onClick={setDarkMode}
 				/>
-				<Button text="Iniciar" onClick={null}/>
+				<Button text={active? "Saber Menos" : "Saber Mais"}
+				onClick={() => {setActive(!active)}}/>
 			</ButtonsLine>
 		</section>
+		{active && <SecInfo />}
+		</>
 	)
 }
 
