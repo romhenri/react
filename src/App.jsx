2a8f1efb5f9c8cbf439ab6d834314df980d65cc9
Rômulo Henri <romulohenri000@gmail.com>
2023-08-12 15:40:39 -0300

    add: State section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Outlet } from 'react-router-dom'
 // Sections
 import SecStart from './layout/SecStart'
 import SecSlide from './layout/SecSlide'
+import SecButton from './layout/SecButton'
 import SecImg from './layout/SecImg'
 import SecData from './layout/SecData'
 // Components
@@ -12,15 +13,14 @@ import SecCounter from './layout/SecCounter'
 
 // Componentes Snippet = "rafce"
 
-
-
 const App = () => {
 	return (
 		<main>
 			<SecStart />
-			<SecCounter />
 			<Outlet />
+			<SecCounter />
 			<SecSlide />
+			<SecButton />
 			<SecImg />
 			<SecData />
 			<Footer />
