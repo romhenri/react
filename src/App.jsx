3c49728bdbea792efe6d287c4701c26af112d092
Rômulo Henri <romulohenri000@gmail.com>
2023-10-16 19:56:27 -0300

    fix: Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,26 @@
 import React from 'react'
-// Components
 import styles from './css/style.css?inline'
-
 // Componentes Snippet = "rafce"
-
 import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, createRoutesFromElements, RouterProvider, BrowserRouter as Router, Routes, Route, Link, Outlet} from 'react-router-dom'
 import ErrorPage from './pages/ErrorPage'
 import About from './pages/About'
 import Contact from './pages/ContactPage'
 import HomePage from './pages/HomePage'
+import SecSingup from './layout/SecSingup'
+import SecLogin from './layout/SecLogin'
 
 const App = () => {
 	return (
 		<main>
 			<Router>
 				<Routes>
-					<Route path='/' element={<HomePage/>}/>
+					<Route path='/' element={<HomePage/>}>
+						<Route path='/' element={<SecSingup />}/>
+						<Route path='/singup' element={<SecSingup />}/>
+						<Route path='/login' element={<SecLogin />}/>
+					</Route>
 					<Route path='about' element={<About/>}/>
-					<Route path='store' element={<About/>}/>
 					<Route path='contact' element={<Contact/>}/>
 					<Route path='error' element={<ErrorPage/>}/>
 					<Route path='*' element={<ErrorPage/>}/>
--- src/pages/LogIn.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const SingIn = () => {
-	return <div>LogIn</div>
-}
-
-export default LogIn
